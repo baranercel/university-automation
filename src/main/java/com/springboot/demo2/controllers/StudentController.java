a3b67ef7934c4ec029189366b14fa8c79cee7916
Baran Erçel <baranercell@gmail.com>
2026-07-15 15:35:25 +0300

    refactor(controller): implement ResponseEntity and HTTP status codes in StudentController

--- src/main/java/com/springboot/demo2/controllers/StudentController.java
+++ src/main/java/com/springboot/demo2/controllers/StudentController.java
@@ -4,6 +4,9 @@ package com.springboot.demo2.controllers;
 import com.springboot.demo2.entities.StudentEntity;
 import com.springboot.demo2.services.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +19,33 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping
-    public List<StudentEntity> getAllStudents(){
-        return studentService.getAllStudents();
+    public ResponseEntity<List<StudentEntity>> getAllStudents(){
+        List<StudentEntity> students = studentService.getAllStudents();
+        return ResponseEntity.ok(students);  // HTTP 200 OK
     }
 
     @GetMapping("/{id}")
-    public StudentEntity getStudentById(@PathVariable Integer id){
-        return studentService.getStudentById(id);
+    public ResponseEntity<StudentEntity> getStudentById(@PathVariable Integer id){
+        StudentEntity student = studentService.getStudentById(id);
+        return ResponseEntity.ok(student);  // HTTP 200 OK
     }
 
     @PostMapping
-    public StudentEntity createStudent(@RequestBody StudentEntity student){
-        return studentService.createStudent(student);
+    public ResponseEntity<StudentEntity> createStudent(@RequestBody StudentEntity student){
+        StudentEntity createdStudent = studentService.createStudent(student);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdStudent);  // HTTP 201 CREATED
     }
 
     @PutMapping("/{id}")
-    public StudentEntity updateStudent(@PathVariable Integer id, @RequestBody StudentEntity student){
-        return studentService.updateStudent(id,student);
+    public ResponseEntity<StudentEntity> updateStudent(@PathVariable Integer id, @RequestBody StudentEntity student){
+        StudentEntity updatedStudent = studentService.updateStudent(id, student);
+        return ResponseEntity.ok(updatedStudent);  // HTTP 200 OK
     }
 
     @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable Integer id){
+    public ResponseEntity<Void> deleteStudent(@PathVariable Integer id){
         studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build(); // HTTP 204 No Content
     }
 
 }
